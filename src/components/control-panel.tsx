0c18988ca2487dca9c5fcf860a95a1d867668bc7
David Graham <david.graham@aize.io>
2026-07-17 20:59:45 +0100

    feat: add fancy animation to title

--- src/components/control-panel.tsx
+++ src/components/control-panel.tsx
@@ -23,12 +23,28 @@ export function ControlPanel({
 }: Props) {
   return (
     <div className="flex h-screen w-100 flex-col gap-4 bg-stone-800 p-4 text-white">
-      <h1
-        className="text-2xl font-semibold"
-        style={{ fontFamily: "AnironBold", letterSpacing: "-0.15em" }}
-      >
-        Battle Map
-      </h1>
+      <header className="relative">
+        <img
+          src="/arrow.png"
+          alt="arrow"
+          className="arrow-1 absolute bottom-2 left-2 w-18 select-none"
+          draggable={false}
+        />
+
+        <img
+          src="/arrow.png"
+          alt="arrow"
+          className="arrow-2 absolute bottom-4 left-3 w-18 select-none"
+          draggable={false}
+        />
+
+        <h1
+          className="flex items-center justify-center gap-2 text-2xl font-semibold text-shadow-md"
+          style={{ fontFamily: "AnironBold", letterSpacing: "-0.15em" }}
+        >
+          Battle Map
+        </h1>
+      </header>
 
       <section className="rounded-lg border border-stone-700 bg-stone-900/40 p-3">
         <h2 className="text-lg">Search</h2>
