125d4044db60b11586e5bbd90df71478cee1b9f0
David Graham <david.graham@aize.io>
2026-07-18 23:59:35 +0100

    feat: add center dot to radius

--- src/components/map-radius.tsx
+++ src/components/map-radius.tsx
@@ -12,25 +12,52 @@ export function MapRadius({ point, size }: Props) {
     steps: 64, // Higher number = smoother circle
     units: "kilometers",
   });
+  const centerPoint = {
+    type: "FeatureCollection" as const,
+    features: [
+      {
+        type: "Feature" as const,
+        geometry: {
+          type: "Point" as const,
+          coordinates: [point.lng, point.lat],
+        },
+        properties: {},
+      },
+    ],
+  };
 
   return (
-    <Source id="search-zone-source" type="geojson" data={circle}>
-      <Layer
-        id="search-zone-fill"
-        type="fill"
-        paint={{
-          "fill-color": "#60758d",
-          "fill-opacity": 0.24,
-        }}
-      />
-      <Layer
-        id="search-zone-stroke"
-        type="line"
-        paint={{
-          "line-color": "#4f647b",
-          "line-width": 2,
-        }}
-      />
-    </Source>
+    <>
+      <Source id="search-zone-source" type="geojson" data={circle}>
+        <Layer
+          id="search-zone-fill"
+          type="fill"
+          paint={{
+            "fill-color": "#60758d",
+            "fill-opacity": 0.24,
+          }}
+        />
+        <Layer
+          id="search-zone-stroke"
+          type="line"
+          paint={{
+            "line-color": "#4f647b",
+            "line-width": 2,
+          }}
+        />
+      </Source>
+
+      <Source id="search-zone-center-source" type="geojson" data={centerPoint}>
+        <Layer
+          id="search-zone-center-dot"
+          type="circle"
+          paint={{
+            "circle-radius": 3,
+            "circle-color": "#4f647b",
+            "circle-opacity": 0.7,
+          }}
+        />
+      </Source>
+    </>
   );
 }
